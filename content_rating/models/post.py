e4225d872e09dc8a26ed4d27b64acaa8197b0487
li-sadeghi <a.sadeghiali@yahoo.com>
2025-02-02 01:54:18 +0330

    fix: Change strategy for calculating avg rating and total ratings for a post for better code performance

--- content_rating/models/post.py
+++ content_rating/models/post.py
@@ -1,6 +1,5 @@
-from django.db import models as django_models
+from django.db import transaction, models as django_models
 from helpers import models as helper_models
-
 
 class Post(helper_models.TimeModel):
     """Represents a post of content rating system.
@@ -13,18 +12,22 @@ class Post(helper_models.TimeModel):
     title = django_models.CharField(max_length=100)
     content = django_models.TextField()
 
+    total_ratings = django_models.PositiveIntegerField(default=0)
+    average_rating = django_models.FloatField(default=0.0)
+
     def __str__(self) -> str:
         return self.title
-
-    @property
-    def total_ratings(self) -> int:
-        """Returns the total number of users who have rated this post."""
-        return self.ratings.count()
-
-    @property
-    def average_rating(self) -> float:
-        """Returns the average rating for the post. If no ratings exist, return 0."""
-        return (
-            self.ratings.aggregate(avg_score=django_models.Avg("score"))["avg_score"]
-            or 0.0
-        )
+    
+    def update_ratings(self):
+        """Update total ratings and average rating."""
+        with transaction.atomic():
+            post = Post.objects.select_for_update().get(id=self.id)
+            total_ratings = post.ratings.count()
+            if total_ratings > 0:
+                avg_rating = post.ratings.aggregate(avg_score=django_models.Avg("score"))["avg_score"]
+            else:
+                avg_rating = 0.0
+            
+            post.total_ratings = total_ratings
+            post.average_rating = avg_rating
+            post.save(update_fields=["total_ratings", "average_rating"])
