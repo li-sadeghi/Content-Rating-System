ac7ffb90f395e9772ed21f627a5e5a57a203611f
li-sadeghi <a.sadeghiali@yahoo.com>
2025-02-02 01:04:42 +0330

    feat: Add admin panel for models of content_rating app.

---
+++ content_rating/admin/__init__.py
@@ -0,0 +1,2 @@
+from content_rating.admin.post_admin import PostAdmin as PostAdmin
+from content_rating.admin.rating_admin import RatingAdmin as RatingAdmin
---
+++ content_rating/admin/post_admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from content_rating import models
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    """Admin interface configuration for the Post model."""
+
+    model = models.Post
+    list_display = (
+        "title",
+        "content",
+        "total_ratings",
+        "average_rating",
+    )
